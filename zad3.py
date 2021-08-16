9b489f8f8ffb21c4c4f7c9202703725d69634263
Furgololo1 <b.furgol62@gmail.com>
2021-08-16 12:11:19 +0200

    First Commit

--- zad3.py
+++ zad3.py
@@ -1,7 +1,11 @@
 
 
-year = 2021
+def funct(year):
 
-for i in range(year, year+20):
-    if i%4 == 0 and i%100 != 0 or i%400 == 0:
-        print(i)
+    for i in range(year, year+20):
+        if i%4 == 0 and i%100 != 0 or i%400 == 0:
+            print(i)
+
+
+
+funct(2021)
